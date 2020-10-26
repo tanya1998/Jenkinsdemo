6bcc8c9559da780e5e4d6d54d7dd5437959698ca
tanya1998 <tanya16108@iiitd.ac.in>
2020-10-26 19:09:01 +0530

    Added required Git repository and pipelines

--- createJobs.groovy
+++ createJobs.groovy
@@ -6,3 +6,17 @@ pipelineJob('pipelineJob') {
         }
     }
 }
+pipelineJob('spring-boot-job') {
+    definition {
+        cpsScm {
+            scm {
+                git {
+                    remote {
+                        url 'https://github.com/tanya1998/SpringBootAWS.git'
+                    }
+                    branch 'master'
+                }
+            }
+        }
+    }
+}
